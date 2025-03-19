20642dd0d20eda2cb69c122441156a03fecaadf6
chenyichen <heyyichenjp@gmail.com>
2025-03-19 14:45:11 +0800

    Fix Login

--- src/components/login.vue
+++ src/components/login.vue
@@ -9,7 +9,7 @@
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Remember me </label>
+    <!-- <label class="form-check-label" for="exampleCheck1">Remember me </label> -->
   </div>
   <button type="submit" class="btn btn-outline-info "  @click="updateusername">登入
   </button>
